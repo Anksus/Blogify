7879562b108eb162a2b2f73b1e748857ac2e461a
Anksus <sankitpramod.min18@itbhu.ac.in>
2020-10-20 18:08:34 +0530

    refactor

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
-const loginRoute = require("./routes/login");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-app.use("/", loginRoute);
+app.use(cors());
+app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Connection with database is established!");
+});
+
+const port = process.env.PORT || 8000;
 app.listen(port, console.log("server running"));
